c4d46510493f51f1bcace35519238c336689bff0
smiti shakya <smitishk12@gmail.com>
2019-09-24 14:17:02 -0500

    completed the assign

--- app.js
+++ app.js
@@ -1,48 +1,41 @@
 const express = require('express');
 const morgan = require('morgan');
-const playstore = require('./playstore');
-
 const app = express();
+
+const playstore = require('./playstore.js');
+
+
 app.use(morgan('common'));
 
+console.log(playstore[0]);
+
 app.get('/apps', (req, res) => {
-    res.status(200);
-    res.send(playstore);
-
-    const { search = "", sort } = req.query;
-
-    if (sort) {
-      if (!['title', 'rank'].includes(sort)) {
-        return res
-          .status(400)
-          .send('Sort must be one of title or rank');
+    const {genres="", sort} = req.query;
+    if(sort){
+      if(!['Rating', 'App'].includes(sort)){
+        return res.status(400).send("Sort must be either rating or app");
       }
     }
-})
 
 
+    let results = playstore.filter(app => 
+      app.Genres.toLowerCase().includes(genres.toLowerCase().trim())
+    );
+    
+    if(sort){
+      results.sort((a,b) => {
+        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+      })
+    }
 
-    //let genres = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'];
-
-
-
-
-
-
-
-
+    res.json(results);
+});
 
 
 app.listen(8000, () => {
     console.log('Server started on PORT 8000');
-  });
+});
 
 
 
 
-
-
-
-
-  //sort  :  rating or app
-  //genres  :  action, puzzle, strategy, casual, arcade, card

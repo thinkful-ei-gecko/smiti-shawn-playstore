64b264f2e0be070d0d4398ea9c94c89df4ac9924
Shawn Collette <shawn.collette@gmail.com>
2019-09-26 21:33:21 -0600

    removed console.log()

--- app.js
+++ app.js
@@ -4,19 +4,17 @@ const app = express();
 
 const playstore = require('./playstore.js');
 
-
 app.use(morgan('common'));
-
-console.log(playstore[0]);
 
 app.get('/apps', (req, res) => {
     const {genres="", sort} = req.query;
     if(sort){
-      if(!['Rating', 'App'].includes(sort)){
-        return res.status(400).send("Sort must be either rating or app");
+      if(!['rating', 'app'].includes(sort.toLowerCase())){
+        return res
+          .status(400)
+          .send("Sort must be either rating or app");
       }
     }
-
 
     let results = playstore.filter(app => 
       app.Genres.toLowerCase().includes(genres.toLowerCase().trim())
@@ -28,14 +26,13 @@ app.get('/apps', (req, res) => {
       })
     }
 
-    res.json(results);
+    return res
+      .status(200)
+      .json(results);
 });
-
 
 app.listen(8000, () => {
     console.log('Server started on PORT 8000');
 });
 
-
-
-
+module.exports = app;
---
+++ test/app.test.js
@@ -0,0 +1,23 @@
+const app = require("../app").default.default.default;
+const { expect } = require("chai");
+const supertest = require("supertest");
+
+describe("GET /apps", () => {
+  it("only accepts the 'sort' parameter as 'rating' or 'app', sends 400 if false", () => {
+    return supertest(app)
+      .get("/apps")
+      .query({ sort: "x" })
+      .expect(400, "Sort must be either rating or app");
+  });
+  it("accepts both upper and lowercase parameters, sends 200 if true", () => {
+    return supertest(app)
+      .get("/apps")
+      .query({ sort: "App" })
+      .expect(function(res) {
+        supertest(app)
+          .get("/apps")
+          .query({ sort: "app" })
+          .expect(200, res.body);
+      })
+  });
+});

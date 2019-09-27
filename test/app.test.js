e2d87b2b0a9a4c4360ae41bed468b6e9f5d0cf66
Shawn Collette <shawn.collette@gmail.com>
2019-09-26 21:35:46 -0600

    added a test

--- test/app.test.js
+++ test/app.test.js
@@ -3,12 +3,14 @@ const { expect } = require("chai");
 const supertest = require("supertest");
 
 describe("GET /apps", () => {
+  
   it("only accepts the 'sort' parameter as 'rating' or 'app', sends 400 if false", () => {
     return supertest(app)
       .get("/apps")
       .query({ sort: "x" })
       .expect(400, "Sort must be either rating or app");
   });
+
   it("accepts both upper and lowercase parameters, sends 200 if true", () => {
     return supertest(app)
       .get("/apps")
